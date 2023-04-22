2c516b5dc65371d42f9c9e159ec76ef18effc140
unknown <vanhuycao2001@gmail.com>
2023-04-22 19:47:15 +0700

    huypromax

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import configViewEngine from './configs/viewEngine';
-import initWebRoutes from './routes/web';
 require('dotenv').config();
 import connectDB from './/config/connectDB';
 import initRoutes from './routes/web';
 
 const app = express();
+const port = process.env.PORT || 2077;
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Accept, Authortization');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Acces-Control-Allow-Credentials', true);
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 configViewEngine(app);
@@ -16,7 +23,6 @@ initRoutes(app);
 //kết nối với cơ sở dữ liệu
 connectDB();
 
-const port = process.env.PORT || 2077;
 app.listen(port, () => {
     console.log('NodeJS is running on localhost:' + port);
 });
